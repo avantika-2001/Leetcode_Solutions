1fd7972a433e18e3b7b61715b9f8a046629552d5
Avantika Singh <81291895+avantika-2001@users.noreply.github.com>
2023-07-08 17:42:10 +0530

    Time: 200 ms (83.16%), Space: 63.5 MB (61.17%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     long long putMarbles(vector<int>& weights, int k) {
-        if (k == 1 || k == weights.size()) {
-            return 0;
-        }
-
         int n = weights.size();
-        vector<long long int>sum1, sum2;
-
-        for (int i = 0; i < weights.size()-1; i++) {
-            sum1.push_back(weights[i] + weights[i+1]);
-            sum2.push_back(weights[i] + weights[i+1]);
+        vector<int>candidates;
+        if(k == 1 || k == n) return 0;
+        for(int i = 0; i < n - 1; i++) {
+            candidates.push_back(weights[i] + weights[i  + 1]);
         }
-
-        sort(sum1.begin(), sum1.end());
-        sort(sum2.rbegin(), sum2.rend());
-
-        long long int ans1 = 0, ans2 = 0;
-        for (int i = 0; i < k-1; i++) {
-            ans1 += sum1[i];
-            ans2 += sum2[i];
+        //[4,8,6]
+        sort(candidates.begin(), candidates.end());
+        //[4,6,8]
+        long long min = 0;
+        long long max = 0;
+        
+        for(int i = 0; i < k - 1; i++) {
+            min += candidates[i]; //10
+            max += candidates[n - i - 2]; //14
         }
-        return (ans2 - ans1);
+        return max - min;
     }
 };
