3297be101241dffcd8e43addfa7cd0fde346f64f
Avantika Singh <81291895+avantika-2001@users.noreply.github.com>
2023-09-08 10:42:57 +0530

    Time: 4 ms (54.55%), Space: 7.6 MB (42.52%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    /*int score(vector<int> &nums,int s,int e,int turn)
+    {
+        if(s==e)
+        {
+            return nums[s];
+        }
+        int chooseStart= turn*nums[s]+ score(nums,s+1,e,-turn);
+        int chooseEnd=turn*nums[e]+ score(nums,s,e-1,-turn);
+        
+        return turn*max(turn*chooseStart, turn*chooseEnd);
+    }
+    bool predictTheWinner(vector<int>& nums) {
+        int n=nums.size();
+        return score(nums,0,n-1,1)>=0;
+    }*/
+    bool predictTheWinner(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        for(int i=0;i<n;i++){
+            dp[i][i]=nums[i];
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                dp[i][j]=max(nums[i]-dp[i+1][j],nums[j]-dp[i][j-1]);                   
+            }
+        }
+        return dp[0][n-1]>=0;
+    }
+};
